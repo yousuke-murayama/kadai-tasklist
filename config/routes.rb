44564b3977e4e2696646ad8a0831d961fa45d732
yousuke.murayama <mura019e@gmail.com>
2021-02-27 05:50:01 +0000

    add User Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-    root to: 'tasks#index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  resources :users, only: [:new, :create]
   resources :tasks
 end
