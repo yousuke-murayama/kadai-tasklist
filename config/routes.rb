bf44f77d8312fdfc32549dab218afd915cc489ef
yousuke.murayama <mura019e@gmail.com>
2021-02-28 00:45:32 +0000

    correct_user show new edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   
